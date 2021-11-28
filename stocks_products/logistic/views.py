897acbc72370912d4ff0a8374cfd56b92296c05d
burkozel <71974079+burkozel@users.noreply.github.com>
2021-11-29 00:52:55 +0300

    Update views.py

--- stocks_products/logistic/views.py
+++ stocks_products/logistic/views.py
@@ -7,7 +7,12 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['address', 'products']
+    filterset_fields = ['products']
